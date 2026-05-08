c095fcd630f0db27676479f2631936dc51c48821
Bam Cortez <2022312593@dhvsu.edu.ph>
2026-05-08 13:51:29 +0800

    feat: externalize profile data and refactor Profile component
    - Move experiences, education, and certifications data to a new profileData.js file
    
    - Update Profile component to import data from the external source for better maintainability

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -6,55 +6,9 @@ import {
   FaBriefcase,
 } from "react-icons/fa";
 
+import { experiences, education, certifications } from "../../data/profileData";
+
 const Profile = () => {
-  const experiences = [
-    {
-      title: "Backend Developer - Intern",
-      company: "Nola Web Solutions",
-      date: "February - April 2026",
-      location: "Pampanga, Philippines",
-      isLatest: true,
-    },
-  ];
-
-  const education = [
-    {
-      school: "Pampanga State University",
-      degree: "Bachelor of Science in Information Technology",
-      date: "2022 - 2026",
-      location: "Cabambangan, Bacolor, Pampanga",
-      awards: [
-        "Dean's List (1st Semester 2022-2023)",
-        "Dean's List (1st Semester 2023-2024)",
-        "President's List (A.Y. 2024-2025)",
-        "Rank 09 Overall in College of Computing Studies (2nd Semester 2024-2025)",
-      ],
-    },
-    {
-      school: "Assumpta Technical High School",
-      degree: "Science, Technology, Engineering, and Mathematics",
-      date: "2020 - 2022",
-      location: "Sta Monica, San Simon, Pampanga",
-    },
-  ];
-
-  const certifications = [
-    {
-      provider: "IBM Skill Build",
-      name: "Cloud Computing Fundamentals",
-    },
-    {
-      provider: "Cisco Networking Academy",
-      items: [
-        "JavaScript Essentials 1",
-        "Introduction to IoT and Digital Transformation",
-      ],
-    },
-    {
-      provider: "TESDA Online Course",
-      name: "Computer System Servicing NCII",
-    },
-  ];
 
   const containerVariants = {
     hidden: { opacity: 0 },
---
+++ frontend/src/data/profileData.js
@@ -0,0 +1,48 @@
+export const experiences = [
+  {
+    title: "Backend Developer - Intern",
+    company: "Nola Web Solutions",
+    date: "February - April 2026",
+    location: "Pampanga, Philippines",
+    isLatest: true,
+  },
+];
+
+export const education = [
+  {
+    school: "Pampanga State University",
+    degree: "Bachelor of Science in Information Technology",
+    date: "2022 - 2026",
+    location: "Cabambangan, Bacolor, Pampanga",
+    awards: [
+      "Dean's List (1st Semester 2022-2023)",
+      "Dean's List (1st Semester 2023-2024)",
+      "President's List (A.Y. 2024-2025)",
+      "Rank 09 Overall in College of Computing Studies (2nd Semester 2024-2025)",
+    ],
+  },
+  {
+    school: "Assumpta Technical High School",
+    degree: "Science, Technology, Engineering, and Mathematics",
+    date: "2020 - 2022",
+    location: "Sta Monica, San Simon, Pampanga",
+  },
+];
+
+export const certifications = [
+  {
+    provider: "IBM Skill Build",
+    name: "Cloud Computing Fundamentals",
+  },
+  {
+    provider: "Cisco Networking Academy",
+    items: [
+      "JavaScript Essentials 1",
+      "Introduction to IoT and Digital Transformation",
+    ],
+  },
+  {
+    provider: "TESDA Online Course",
+    name: "Computer System Servicing NCII",
+  },
+];
